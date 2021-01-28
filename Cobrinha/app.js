15a94d4dc48fb36a53d0f862ca3bc8eb08aacada
Marcelo <marcelojunqueiraf@gmail.com>
2021-01-28 15:25:18 -0300

    one file

--- Cobrinha/app.js
+++ Cobrinha/app.js
@@ -1,7 +1,6 @@
 const http = require('http');
-
 let app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(index)
     res.end('Hello World!\n');
 });
 
